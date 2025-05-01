534f1b8ccaaa0184040b5274a00675a689d1e6dd
thestudent101 <thestudent101@users.noreply.github.com>
2025-05-01 22:48:04 +0200

    Enviroments and keys update

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api.qa.resourceio.app/v2',
-  apiKey: 'vO9UOq5Js5epm2lDHi033q9UWZkGUxv2UMq7J793',
+  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seGpzc3FzdW5hcWRqdWVyeG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY5OTIsImV4cCI6MjA2MTY3Mjk5Mn0.DoN4ZOrf_v7SkQ6UaVYH8mItqI6GP_1D2_SHMfcfUYk',
   privacyPolicyUrl: 'https://resourceio.app/privacy-notice/',
   termsUrl: 'https://resourceio.app/terms-and-conditions/',
   supabaseUrl: 'https://mlxjssqsunaqdjuerxoq.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlc291cmNlaW8iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNDU4MjQwMCwiZXhwIjoxNzE0NTgyNDAwfQ.examplekey',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seGpzc3FzdW5hcWRqdWVyeG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY5OTIsImV4cCI6MjA2MTY3Mjk5Mn0.DoN4ZOrf_v7SkQ6UaVYH8mItqI6GP_1D2_SHMfcfUYk',
   versionString: "QA-2.3.0",
   peachUrl: 'https://test.oppwa.com/v1/paymentWidgets.js?checkoutId='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.resourceio.app/v2',
-  apiKey: '04PCpMZcVM7RJ9hMtU6BT5JzfipU1ZE65XHAQWFf',
+  apiKey: '04PCpMZcVM7RJ9hMtU6BT5JzfipU1ZE65XHAQWFfeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seGpzc3FzdW5hcWRqdWVyeG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY5OTIsImV4cCI6MjA2MTY3Mjk5Mn0.DoN4ZOrf_v7SkQ6UaVYH8mItqI6GP_1D2_SHMfcfUYk',
   privacyPolicyUrl: 'https://resourceio.app/privacy-notice/',
   termsUrl: 'https://resourceio.app/terms-and-conditions/',
   supabaseUrl: 'https://mlxjssqsunaqdjuerxoq.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlc291cmNlaW8iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNDU4MjQwMCwiZXhwIjoxNzE0NTgyNDAwfQ.examplekey',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seGpzc3FzdW5hcWRqdWVyeG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY5OTIsImV4cCI6MjA2MTY3Mjk5Mn0.DoN4ZOrf_v7SkQ6UaVYH8mItqI6GP_1D2_SHMfcfUYk',
   versionString: "DEV-2.3.0",
   peachUrl: 'https://test.oppwa.com/v1/paymentWidgets.js?checkoutId='
 };
